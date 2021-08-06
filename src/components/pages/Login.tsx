07f9e96231a9ff466b1e82b4d4818e5034a31a29
sea-kai <76773825+sea-kai@users.noreply.github.com>
2021-08-06 23:58:49 +0900

    feat: ログインカード追加

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,27 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack
+} from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Login: VFC = memo(() => {
-  return <p>ログインページです</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack>
+          <Input placeholder="ユーザーID" />
+          <Button>ログイン</Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
